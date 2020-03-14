103aeb4c5bb48e6471213ea64502255293a239e3
Derfdyderf <60189750+Derfdyderf@users.noreply.github.com>
2020-03-13 21:58:10 -0700

    first part of problem_2

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MaxHeap implements Heap {
     int size;
 
@@ -10,31 +13,70 @@ public class MaxHeap implements Heap {
 
     // build a heap based on data
     // to be implemented in O(nlogn)
-    public void MaxHeapLogN(Integer[] data) {
-        // homework
+    public void MaxHeapnLogN(Integer[] data) {
+        Arrays.sort(data, Collections.reverseOrder());
+
+        size = data.length;
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
-        // homework
+        size = data.length;
+
+        for (int i = (size/2)-1; i >= 0; i--)
+            heapifyUp(data, size, i);
     }
 
     // add an item to the heap
     public boolean add(Integer item) {
-        // homework
-        return false;
+        size++;
+        data[size-1] = item;
+        heapifyUp(data, size, size-1);
+        return true;
     }
 
     // return the max item in the heap
     public Integer get() {
-        // homework
-        return null;
+
+        return data[0];
     }
 
     // remove the root item
     public Integer pop() {
-        // homework
-        return null;
+        Integer pop = data[0];
+        data[0] = data[size-1];
+        size--;
+        heapifyUp(data, size, 0);
+        return pop;
+    }
+
+    public void swap(Integer[] data, int i,int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+
+    /*
+    this was very much so influenced by the heapify method on GeeksForGeeks
+     */
+    public void heapifyUp(Integer[] data, int s, int val) {
+        int bigBoy = val;
+        int left = 2*val+1;
+        int right = 2*val+2;
+
+        if ((left < s) && (data[left] > data[bigBoy])) {
+            bigBoy = left;
+        }
+
+        if ((right < s) && (data[right] > data[bigBoy])) {
+            bigBoy = right;
+        }
+
+        if (!(bigBoy == val)) {
+            swap(data, val, bigBoy);
+            heapifyUp(data, s, bigBoy);
+        }
     }
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,5 +1,55 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MaxHeapTest {
-    // homework
+    @Test
+    public void MaxHeapLogNT() {
+        Integer[] input = {1,2,3,4,5,6,7,8,9};
+        Integer[] expected = {9,8,7,6,5,4,3,2,1};
+        MaxHeap check = new MaxHeap(10);
+        check.MaxHeapnLogN(input);
+
+        assertArrayEquals(expected,input);
+    }
+
+    @Test
+    public void MaxHeapNT() {
+        Integer[] input = {1,2,3,4,5,6,7,8,9};
+        Integer[] expected = {9,8,7,4,5,6,3,2,1};
+        MaxHeap check = new MaxHeap(10);
+        check.MaxHeapN(input);
+
+        assertArrayEquals(expected,input);
+    }
+
+    /**
+     * get and pop are both tested in this method
+     */
+    @Test
+    public void MaxHeapGetAndPopT() {
+        Integer[] input = {1,2,3,4,5,6,7,8,9};
+        MaxHeap check = new MaxHeap(10);
+        check.MaxHeapnLogN(input);
+
+        for (int i = 9; i <= 0; i--) {
+            int get = check.get();
+            int pop = check.pop();
+            assertEquals(get, pop);
+            assertEquals(i, pop);
+        }
+    }
+
+    @Test
+    public void MaxHeapAdd() {
+        Integer[] input = {9,8,7,6,5,4,3,2,1};
+        Integer[] expected = {9,8,7,6,5,4,3,2,1};
+
+        MaxHeap check = new MaxHeap(10);
+        for( int i = 0; i < input.length; i++) {
+            check.add(input[i]);
+        }
+
+        assertArrayEquals(expected,input);
+    }
 }
